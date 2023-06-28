d3ea75e2b5525bba44833c07e39f176b75c39b8d
Andrea Higley <104789558+dandy1ion@users.noreply.github.com>
2023-06-27 18:04:12 -0600

    final tweaks

--- Portfolio23/js/script.js
+++ Portfolio23/js/script.js
@@ -32,3 +32,30 @@ window.onscroll = () => {
 	menuIcon.classList.remove("bx-x");
 	navbar.classList.remove("active");
 };
+
+// email functionality
+function sendMail() {
+	let params = {
+		name: document.getElementById("name").value,
+		email: document.getElementById("email").value,
+		number: document.getElementById("number").value,
+		subject: document.getElementById("subject").value,
+		message: document.getElementById("message").value,
+	};
+
+	const serviceID = "service_4j1t30m";
+	const templateID = "template_8le21od";
+
+	emailjs
+		.send(serviceID, templateID, params)
+		.then((res) => {
+			document.getElementById("name").value = "";
+			document.getElementById("email").value = "";
+			document.getElementById("number").value = "";
+			document.getElementById("subject").value = "";
+			document.getElementById("message").value = "";
+			console.log(res);
+			alert("your message sent successfully");
+		})
+		.catch((err) => console.log(err));
+}
